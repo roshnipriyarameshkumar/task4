d3ac51bab015746e80293c237cff32339db32498
Roshnipriya <127740197+roshnipriyarameshkumar@users.noreply.github.com>
2025-02-06 14:12:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ echo hi123
     docker login -u roshni178 -p Foxowlviya@21
     docker tag test roshni178/task4
     docker push roshni178/task4
-    kubectl create deployment task45 --image=roshni178/task4 --port=80
-    kubectl expose deployment task45 --type=NodePort --port=80
+  
    
     
